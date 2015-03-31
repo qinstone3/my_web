23bf79663b0e7b378b0d09ca393c51ed494a4ffc
qinstone3 <rednumber@RedNumberdeMacBook-Air.local>
2015-03-31 22:18:27 +0800

    fix test,sql

--- test.sql
+++ test.sql
@@ -2,7 +2,9 @@ drop database if exists test;
 create database test;
 use test;
 
-create table user(
+grant select,update,insert,delete on test.* to 'root'@'localhost' identified by '123456';
+
+create table users(
 	id varchar(50) primary key not null,
 	name varchar(50) not null,
 	email varchar(50) not null,
@@ -10,3 +12,8 @@ create table user(
 	admin bool not null,
 	create_at real not null
 ) engine=innodb default charset=utf8;
+
+insert into users (id,name,email,password,admin,create_at) values 
+	('1432523','qinlei','qinlei@qinlei.com','123456',1,234234);
+
+select * from users;
